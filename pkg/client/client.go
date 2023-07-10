895b94144d270fb17e8d20dfaae2d77e54e49b75
Krishna Iyer Easwaran <krishna@thethingsindustries.com>
2023-07-10 17:17:12 +0200

    Add HTTP client wrapper

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -13,3 +13,65 @@
 // limitations under the License.
 
 package client
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+const httpTimeout = 10 * time.Second
+
+// Config is the client configuration.
+type Config struct {
+	APIKey string `name:"api-key" description:"The GitHub API key"`
+}
+
+// Client is a GitHub client.
+type Client struct {
+	config *Config
+	client *http.Client
+}
+
+// NewClient creates a new client.
+func NewClient(config *Config) *Client {
+	return &Client{
+		config: config,
+		client: &http.Client{
+			Timeout: httpTimeout,
+		},
+	}
+}
+
+// Do executes a request.
+func (c *Client) Do(ctx context.Context, method string, url string, body []byte) ([]byte, error) {
+	req, err := http.NewRequestWithContext(ctx, method, url, bytes.NewReader(body))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.config.APIKey))
+	req.Header.Set("Accept", "application/vnd.github.v3+json")
+	req.Header.Set("User-Agent", "ghpm")
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	switch {
+	case resp.StatusCode >= 200 && resp.StatusCode < 300:
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+		return body, nil
+	case resp.StatusCode == http.StatusNotFound:
+		return nil, fmt.Errorf("not found")
+	default:
+		return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+}
